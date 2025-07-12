554079a1916d7454bb8974cd8481d8de7bf45b48
sanskar <sharmasanskar7201@gmail.com>
2025-07-12 20:14:48 +0530

    first commit

--- server.js
+++ server.js
@@ -16,10 +16,14 @@ app.set('views', './views');
 const UserRoute =  require("./routes/user");
 const HomeRoute = require("./routes/home");
 const Userlogin = require("./routes/Userlogin");
+const UserSignup = require("./routes/UserSignup");
+const UserResetPass = require("./routes/ForgetPass");
 
 app.use("/user" , UserRoute);
 app.use("/home", HomeRoute);
 app.use("/login" , Userlogin);
+app.use("/Signup" , UserSignup);
+app.use("/forgetPass" , UserResetPass);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
--- tailwind.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    content: ['./views/**/*.ejs'],
-    theme: { extend: {} },
-    plugins: []
-};
